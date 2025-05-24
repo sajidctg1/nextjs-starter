7c989861c705e7606161465466668849cd4d11c2
sajidctg1 <sajid.ctg03@gmail.com>
2025-05-24 18:19:32 +0600

    fix(drizzle.config.ts): fix schema files path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schemas.ts",
+  schema: "./src/server/db/schemas/index.ts",
   out: "migrations",
   dialect: "sqlite",
   dbCredentials: {
