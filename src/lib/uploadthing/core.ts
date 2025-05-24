e704458a7e7c59f21772e78b16d929452e065987
sajidctg1 <sajid.ctg03@gmail.com>
2025-05-24 15:40:52 +0600

    fix(uploadthing/core): add authentication and rename avatar router

--- src/lib/uploadthing/core.ts
+++ src/lib/uploadthing/core.ts
@@ -3,27 +3,27 @@ import "server-only";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
-const f = createUploadthing();
+import { authenticate } from "~/server/helpers";
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
+  avatarUploader: f({
     image: {
       /**
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
-      maxFileSize: "4MB",
+      maxFileSize: "1MB",
       maxFileCount: 1,
     },
   })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
+    .middleware(async () => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const { user } = await authenticate();
 
       // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
